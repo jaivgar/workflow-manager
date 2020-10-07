110ab878ea90d5bf4f10a1ef3130ec21eabd477a
jaivgar <jaime.garcia@ltu.se>
2020-10-07 15:30:33 +0200

    Fix method to test core systems at initialization
    The checkCoreSystems method now works as expected, not only when the
    first try of communication is succesful. When core system not present,
    it will keep trying to contact each core system for a certain amount of
    minutes, sending a new request every 5 seconds.
    
    Problem was with exception not being caught in HttpClientRequest. The
    exception thrown, ConnectException, is not known at compile time as it
    comes inside a Future. It is not thrown by any method directly called
    in my code, but from the netty library executing the send request in
    another thread. Therefore I was not catching it, only the ones I knew
    of.
    
    Solution involved the use of methods provided to handle Futures in
    Kalix library, and the creation of a new private class to handle the
    time duration of these requests. This class was created to include all
    logic inside the lambda functions used in the methods for Futures, as
    lambda functions can only access static and instance variables or final
    local variables.

--- workflow-manager/src/main/java/se/ltu/workflow/manager/WManagerMain.java
+++ workflow-manager/src/main/java/se/ltu/workflow/manager/WManagerMain.java
@@ -1,5 +1,6 @@
 package se.ltu.workflow.manager;
 
+import java.net.ConnectException;
 import java.net.InetSocketAddress;
 import java.time.Duration;
 import java.util.ArrayList;
@@ -229,44 +230,53 @@ public class WManagerMain {
     }
     
     private static void checkCoreSystems(HttpClient  client, int minutes) throws InterruptedException, TimeoutException {
-
+        
+        TimeCount timer = new TimeCount(Duration.ofMinutes(minutes));
+        
         // Service Registry
         final String serviceRegistryAddres = props.getProperty("sr_address","127.0.0.1");
         final int serviceRegistryPort = props.getIntProperty("sr_port", 8443);
         final var serviceRegistrySocketAddress = new InetSocketAddress(serviceRegistryAddres, serviceRegistryPort);
-        int nCalls = 0;
-        // 5 seconds * 12 = 1 minute
-        while(nCalls/12 < minutes) {
-            // Send GET request to echo service
+        logger.info("Testing connection with Service Registry");
+        
+        // Send GET request to echo service until reply or timeout
+        while(!timer.timeout()) {
             try {
-                /* TODO: Throws Exception in spawn thread when request does not find target, exiting application (How can I catch it?)
-                 * io.netty.channel.AbstractChannel$AnnotatedConnectException: finishConnect(..) failed: Connection refused: /127.0.0.1:8443
-                 */
+                // Future throws exception (java.net.ConnectException) when server not available
                 String result = client.send(serviceRegistrySocketAddress, new HttpClientRequest()
-                        .method(HttpMethod.GET)
-                        .uri("serviceregistry/echo"))
-                        .flatMap(response -> response.bodyAsString())
-                        .await(Duration.ofSeconds(5));
-                // If exception is not thrown, request was successful so end loop
-                nCalls = Integer.MAX_VALUE;
-                if (!result.isEmpty()) {
-                    logger.info("Service Registry core system is reachable.");
-                }
-                else{
-                    logger.warn("Service Registry core system was reached, but Echo message was empty!");
-                }
+                    .method(HttpMethod.GET)
+                    .uri("serviceregistry/echo"))
+                    .flatMap(response -> response.bodyAsString())
+                    .ifSuccess(response -> {
+                        // If exception is not thrown, request was successful so end loop
+                        timer.endCount();
+                        if (!response.isEmpty()) {
+                            logger.info("Service Registry replied, core system is reachable");
+                        }
+                        else{
+                            logger.warn("Service Registry core system was reached, but Echo message was empty!");
+                        }
+                    })
+                    .flatMapCatch(ConnectException.class, exception -> {
+                        int retryPeriodSeconds = 5;
+                        if(timer.discountAndWait(Duration.ofSeconds(retryPeriodSeconds))) {
+                            logger.info("Service Registry core system is not reachable, retry in " 
+                                    + retryPeriodSeconds + " seconds");
+                            return Future.done();
+                        }
+                        else {
+                            logger.error("Service Registry mandatory core system is not reachable, "
+                                    + "Arrowhead local cloud incomplete");
+                            var e = new TimeoutException();
+                            e.addSuppressed(exception);
+                            throw e;
+                        }
+                    })
+                    .await();
             }
             catch (InterruptedException e) {
                 logger.error("Workflow Manager interrupted when waiting for Service Regsitry echo message");
                 throw e;
-            }
-            catch (TimeoutException e) {
-                nCalls ++;
-                if(nCalls/12 >= minutes) {
-                    logger.error("Service Registry mandatory core system is not reachable, Arrowhead local cloud incomplete!");
-                    throw e;
-                }
-                logger.info("Waiting for ServiceRegistry to be available ...");
             }
         }
 
@@ -274,35 +284,45 @@ public class WManagerMain {
         final String OrchestratorAddres = props.getProperty("orch_address","127.0.0.1");
         final int OrchestratorPort = props.getIntProperty("orch_port", 8441);
         final var OrchestratorAddress = new InetSocketAddress(OrchestratorAddres, OrchestratorPort);
-        nCalls = 0;
+        logger.info("Testing connection with Orchestrator");
         
-        while(nCalls/12 < minutes) {
-         // Send GET request to echo service
+        timer.resetCount(Duration.ofMinutes(minutes));
+        while(!timer.timeout()) {
             try {
                 String result = client.send(OrchestratorAddress, new HttpClientRequest()
-                        .method(HttpMethod.GET)
-                        .uri("orchestrator/echo"))
-                        .flatMap(response -> response.bodyAsString())
-                        .await(Duration.ofSeconds(5));
-                nCalls = Integer.MAX_VALUE;
-                if (!result.isEmpty()) {
-                    logger.info("Orchestrator core system is reachable.");
-                }
-                else{
-                    logger.warn("Orchestrator core system was reached, but Echo message was empty!");
-                }
+                    .method(HttpMethod.GET)
+                    .uri("orchestrator/echo"))
+                    .flatMap(response -> response.bodyAsString())
+                    .ifSuccess(response -> {
+                        // If exception is not thrown, request was successful so end loop
+                        timer.endCount();
+                        if (!response.isEmpty()) {
+                            logger.info("Orchestrator replied, core system is reachable");
+                        }
+                        else{
+                            logger.warn("Orchestrator core system was reached, but Echo message was empty!");
+                        }
+                    })
+                    .flatMapCatch(ConnectException.class, exception -> {
+                        int retryPeriodSeconds = 5;
+                        if(timer.discountAndWait(Duration.ofSeconds(retryPeriodSeconds))) {
+                            logger.info("Orchestrator core system is not reachable, retry in " 
+                                    + retryPeriodSeconds + " seconds");
+                            return Future.done();
+                        }
+                        else {
+                            logger.error("Orchestrator mandatory core system is not reachable, "
+                                    + "Arrowhead local cloud incomplete");
+                            var e = new TimeoutException();
+                            e.addSuppressed(exception);
+                            throw e;
+                        }
+                    })
+                    .await();
             }
             catch (InterruptedException e) {
                 logger.error("Workflow Manager interrupted when waiting for Orchestrator echo message");
                 throw e;
-            }
-            catch (TimeoutException e) {
-                nCalls ++;
-                if(nCalls/12 >= minutes) {
-                    logger.error("Orchestrator mandatory core system is not reachable, Arrowhead local cloud incomplete!");
-                    throw e;
-                }
-                logger.info("Waiting for Orchestrator to be available ...");
             }
         }
         
@@ -311,41 +331,97 @@ public class WManagerMain {
             final String AuthorizationAddres = props.getProperty("auth_address","127.0.0.1");
             final int AuthorizationPort = props.getIntProperty("auth_port", 8445);
             final var AuthorizationSocketAddress = new InetSocketAddress(AuthorizationAddres, AuthorizationPort);
-            nCalls = 0;
             
-            while(nCalls/12 < minutes) {
+            timer.resetCount(Duration.ofMinutes(minutes));
+            while(!timer.timeout()) {
              // Send GET request to echo service
                 try {
                     String result = client.send(AuthorizationSocketAddress, new HttpClientRequest()
-                            .method(HttpMethod.GET)
-                            .uri("authorization/echo"))
-                            .flatMap(response -> response.bodyAsString())
-                            .await(Duration.ofSeconds(5));
-                    nCalls = Integer.MAX_VALUE;
-                    if (!result.isEmpty()) {
-                        logger.info("Authorization core system is reachable.");
-                    }
-                    else{
-                        logger.warn("Authorization core system was reached, but Echo message was empty!");
-                    }
+                        .method(HttpMethod.GET)
+                        .uri("authorization/echo"))
+                        .flatMap(response -> response.bodyAsString())
+                        .ifSuccess(response -> {
+                            // If exception is not thrown, request was successful so end loop
+                            timer.endCount();
+                            if (!response.isEmpty()) {
+                                logger.info("Authorization replied, core system is reachable");
+                            }
+                            else{
+                                logger.warn("Authorization core system was reached, but Echo message was empty!");
+                            }
+                        })
+                        .flatMapCatch(ConnectException.class, exception -> {
+                            int retryPeriodSeconds = 5;
+                            if(timer.discountAndWait(Duration.ofSeconds(retryPeriodSeconds))) {
+                                logger.info("Authorization core system is not reachable, retry in " 
+                                        + retryPeriodSeconds + " seconds");
+                                return Future.done();
+                            }
+                            else {
+                                logger.error("Authorization mandatory core system is not reachable, "
+                                        + "Arrowhead local cloud incomplete");
+                                var e = new TimeoutException();
+                                e.addSuppressed(exception);
+                                throw e;
+                            }
+                        })
+                        .await();
                 }
                 catch (InterruptedException e) {
                     logger.error("Workflow Manager interrupted when waiting for Authorization echo message");
                     throw e;
                 }
-                catch (TimeoutException e) {
-                    nCalls ++;
-                    if(nCalls/12 >= minutes) {
-                        logger.error("Authorization mandatory core system is not reachable, Arrowhead local cloud incomplete!");
-                        throw e;
-                    }
-                    logger.info("Waiting for Authorization to be available ...");
-                    
-                    throw e;
-                }
             }
-            
         }
-
     }
+    
+    private static class TimeCount {
+        private long timeLeftInCount;
+        
+        public TimeCount(Duration minutes) {
+            if(minutes.getSeconds() < 60) {
+                throw new IllegalArgumentException("The minimun amount of time accepted is 1 minute (60 seconds)");
+            }
+            timeLeftInCount = minutes.getSeconds();
+        }
+        
+        public void resetCount(Duration minutes) {
+            if(minutes.getSeconds() < 60) {
+                throw new IllegalArgumentException("The minimun amount of time accepted is 1 minute (60 seconds)");
+            }
+            timeLeftInCount = minutes.getSeconds();
+        }
+        
+        /**
+         * Discount/subtract an amount of time (greater than a second) from this TimeCount, and sleep the thread
+         * for that same amount 
+         * 
+         * @param seconds time to be subtracted from the internal time count and keep thread slept
+         * @return true when there is still time in the count after the operation of subtraction
+         * @throws InterruptedException if thread interrupted while
+         */
+        public Boolean discountAndWait(Duration seconds) throws InterruptedException {
+            timeLeftInCount -= seconds.getSeconds();
+            try {
+                Thread.sleep(seconds.getSeconds() * 1000);
+            } catch (InterruptedException e) {
+                throw e;
+            }
+            return timeLeftInCount > 0;
+        }
+        
+        /**
+         * Checks if the initial time entered has been used
+         * 
+         * @return true when the time left is less or equal than zero
+         */
+        public Boolean timeout() {
+            return timeLeftInCount <= 0;
+        }
+        
+        public void endCount() {
+            timeLeftInCount = 0;
+        }
+    }
+
 }
